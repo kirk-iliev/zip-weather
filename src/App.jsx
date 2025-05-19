e7521d62f6c897b7743e9ba1f57d8a148cf8bf9a
Kirk Iliev <kirkiliev0@gmail.com>
2025-05-19 01:14:54 -0700

    Componentization and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import zipData from './zipcodes.json'
+import zipData from './USCities.json'
 
 function SearchBar({inputValue, setInputValue}) {
   return (
@@ -15,25 +15,73 @@ function SearchBar({inputValue, setInputValue}) {
 
 function SearchButton({onClick}) {
   return (
-    <button
-      onClick={onClick}
-    >Search</button>
+    <button onClick={onClick}>
+      Search
+    </button>
   )
+}
+
+function ForecastColumnHourly({hourlyPeriods}) {
+  return (
+    <div className='forecast-column'>
+      <h3>Hourly Forecast</h3>
+      {hourlyPeriods.length > 0 && (
+        hourlyPeriods.slice(0,24).map((period, index) => (
+          <div key={index}>
+            <p>{
+              new Date(period.startTime).toLocaleTimeString([], {
+                hour: 'numeric', minute: '2-digit'})
+                + ": " + period.temperature + "° F"
+              }
+            </p>
+            <p>
+              {period.shortForecast}
+            </p>
+            <img src={period.icon} alt="weather icon"/>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+function ForecastColumnSevenDay({detailedPeriods}) {
+  return (
+    <div className='forecast-column'>
+      {detailedPeriods.length > 0 && (
+        detailedPeriods.map((period, index) => (
+          <div key = {index}>
+            <h3> {period.name + ": " + period.temperature + "° F"} </h3>
+            <p> {period.detailedForecast}</p>
+            <img src={period.icon}/>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+function CityName({ inputValue }) {
+  const cityMatch = zipData.find(zips => zips.zip_code.toString() === inputValue.trim())
+  if (cityMatch) {
+    return (
+      <h3>{cityMatch.city}, {cityMatch.state}</h3>
+    )
+  }
 }
 
 export default function App() {
 
-  const [weatherData, setWeatherData] = useState(null)
   const [detailedPeriods, setDetailedPeriods] = useState([])
   const [hourlyPeriods, setHourlyPeriods] = useState([])
   const [inputValue, setInputValue] = useState('')
 
   function getLatLong(inputValue) {
-    const searchMatch = zipData.find(zips => zips.ZIP.toString() === inputValue.trim())
+    const searchMatch = zipData.find(zips => zips.zip_code.toString() === inputValue.trim())
     console.log(searchMatch)
     if (searchMatch) {
-      const lat = searchMatch.LAT
-      const long = searchMatch.LNG
+      const lat = searchMatch.latitude
+      const long = searchMatch.longitude
       fetchWeatherData(lat, long)
     } else {
       alert('Invalid zip code')
@@ -45,15 +93,14 @@ export default function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
-        const forecastDetailedURL = data.properties.forecast
+        const forecastSevenDayURL = data.properties.forecast
         const forecastHourlyURL = data.properties.forecastHourly
         return Promise.all([
-          fetch(forecastDetailedURL).then((response) => response.json()),
+          fetch(forecastSevenDayURL).then((response) => response.json()),
           fetch(forecastHourlyURL).then((response) => response.json()),
         ])
       })
       .then(([dailyData, hourlyData]) => {
-        setWeatherData(dailyData)
         setDetailedPeriods(dailyData.properties.periods)
         setHourlyPeriods(hourlyData.properties.periods)
       })
@@ -64,47 +111,28 @@ export default function App() {
   return (
 
     <div className='app-container'>
-        <div className='weather-container'>
-          <h1>Weather</h1>
-          <div className='zip-search'>
-            <SearchBar
-              setInputValue={setInputValue}
-              inputValue={inputValue}
-            />
-            <SearchButton onClick={() => getLatLong(inputValue)}/>
-          </div>
-          {/* Split the weather container into two columns
-              one for hourly one for 7-day forecast */}
-          <div className='forecast-columns'>
-            <div className='forecast-column'>
-
-              {weatherData && weatherData.properties && hourlyPeriods && (
-
-                hourlyPeriods.slice(0,24).map((period, index) => (
-
-                  <div key = {index}>
-                    <p> {period.startTime.slice(11, 16) + ": " + period.temperature + "° F"} </p>
-                    {/* <p> {period.detailedForecast}</p> */}
-                    <img src={period.icon}/>
-                  </div>
-                ))
-              )}
-            </div>
-            <div className='forecast-column'>
-              {weatherData && weatherData.properties && detailedPeriods && (
-                detailedPeriods.map((period, index) => (
-                  <div key = {index}>
-                    <h3> {period.name + ": " + period.temperature + "° F"} </h3>
-                    <p> {period.detailedForecast}</p>
-                    <img src={period.icon}/>
-                  </div>
-                ))
-              )}
-            </div>
-
-          </div>
-
+      <div className='weather-container'>
+        <h1>Weather</h1>
+        <CityName
+          inputValue={inputValue}
+        />
+        <div className='search-container'>
+          <SearchBar
+            setInputValue={setInputValue}
+            inputValue={inputValue}
+          />
+          <SearchButton onClick={() => getLatLong(inputValue)}/>
         </div>
+        {/* Two-column forecast container */}
+        <div className='forecast-container'>
+          <ForecastColumnHourly
+            hourlyPeriods={hourlyPeriods}
+          />
+          <ForecastColumnSevenDay
+            detailedPeriods={detailedPeriods}
+          />
+        </div>
+      </div>
     </div>
   )
 }
